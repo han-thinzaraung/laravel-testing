26104423fadba7693838a7655c886af49499b2a3
han-thinzaraung <hanthinzaraung@gicjp.com>
2024-03-20 17:08:47 +0630

    test

---
+++ tests/Feature/ProductsTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Product;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProductsTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_homepage_contains_empty_table()
+    {
+        $response = $this->get('/products');
+
+        $response->assertStatus(200);
+        $response->assertSee(__('No products found'));
+    }
+    public function test_homepage_contains_non_empty_table()
+    {
+        Product::create([
+            'name' => 'product1',
+            'price' => 123
+        ]);
+        $response = $this->get('/products');
+
+        $response->assertStatus(200);
+        $response->assertDontSee(__('No products found'));
+    }
+}
